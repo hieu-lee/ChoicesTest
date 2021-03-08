9107fbe4f592804a11a6c26ec4aea5c3f8e86539
Hieu Le <hieuld1009@gmail.com>
2021-03-08 16:11:06 +0700

    first push

---
+++ ChoicesTest/Models/QuestionInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoicesTest.Models
+{
+    public class QuestionInfo
+    {
+        public int number { get; set; }
+        public string question { get; set; }
+        public string answerA { get; set; }
+        public string answerB { get; set; }
+        public string answerC { get; set; }
+        public string answerD { get; set; }
+        public string answer { get; set; }
+    }
+}
