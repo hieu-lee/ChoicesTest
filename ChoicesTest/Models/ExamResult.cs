d5610a85674e069c7a7818cc41225e6431c79b81
Hieu Le <hieuld1009@gmail.com>
2021-03-10 15:16:36 +0700

    score-saving update

---
+++ ChoicesTest/Models/ExamResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChoicesTest.Models
+{
+    public class ExamResult
+    {
+        public string examCode { get; set; }
+        public decimal score { get; set; }
+        public string time { get; set; }
+    }
+}
