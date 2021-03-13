73d8a22f898a9747ae4c2f139fb1e5c79d26199e
Hieu Le <hieuld1009@gmail.com>
2021-03-13 12:11:49 +0700

    add exam showing key option

--- ChoicesTest/Data/AppData.cs
+++ ChoicesTest/Data/AppData.cs
@@ -15,6 +15,20 @@ namespace ChoicesTest.Data
         public SqliteConnection connection= new SqliteConnection("Data Source=exams.db");
         public bool inited = false;
 
+        public async Task<bool> GetExamPrivacyAsync(string ExamCode)
+        {
+            var task = connection.OpenAsync();
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = $"SELECT privacy FROM exam_collection WHERE code = '{ExamCode}'";
+            await task;
+            using var rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                return (rdr.GetInt16(0) == 1);
+            }
+            return false;
+        }
+
         public async Task InitializeAsync()
         {
             var task = connection.OpenAsync();
@@ -23,7 +37,7 @@ namespace ChoicesTest.Data
             var myCmd3 = connection.CreateCommand();
             myCmd2.CommandText = "CREATE TABLE IF NOT EXISTS accounts(username TEXT PRIMARY KEY, password TEXT, name TEXT, admin INTEGER);";
             myCmd3.CommandText = "CREATE TABLE IF NOT EXISTS history(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, exam TEXT, score REAL, time TEXT);";
-            myCmd.CommandText = "CREATE TABLE IF NOT EXISTS exam_collection(code TEXT PRIMARY KEY, name TEXT, time INTEGER, label TEXT);";
+            myCmd.CommandText = "CREATE TABLE IF NOT EXISTS exam_collection(code TEXT PRIMARY KEY, name TEXT, time INTEGER, label TEXT, privacy INTEGER);";
             await task;
             myCmd.ExecuteNonQuery();
             myCmd2.ExecuteNonQuery();
